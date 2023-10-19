e148b7c079ead9bcf4740ea7a998a0f277836b5c
MmmNaad <125236702+sicmnvio@users.noreply.github.com>
2023-10-19 10:50:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,6 @@ curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
 $res = curl_exec($ch);
 <?php
 $token = 'YOUR_BOT_TOKEN';
-$chat_id = 'TARGET_CHAT_ID';
 $message_id = 'MESSAGE_ID';
 
 file_get_contents("https://api.telegram.org/bot$token/deleteMessage?chat_id=$chat_id&message_id=$message_id");
